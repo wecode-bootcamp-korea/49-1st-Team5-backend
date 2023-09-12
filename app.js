6aeef03766a82543899bff8ac310e0537297e002
ayoung <say199532@gmail.com>
2023-09-12 18:08:54 +0900

    initial setting_수정1

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ const port = process.env.PORT || 8000
 
 const { DataSource } = require('typeorm');
 
-const myDataSource = new DataSource({
-  type: process.env.TYPEORM_CONNECTION,
-  host: process.env.TYPEORM_HOST,
-  port: process.env.TYPEORM_PORT,
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE
+const AppDataSource = new DataSource({
+  type: process.env.DB_CONNECTION,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 })
 
 myDataSource.initialize()
@@ -28,15 +28,14 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan('combined'))
 
-
 app.get('/', async (req, res) => {
   try {
     return res.status(200).json({ "message": "Welcome to 49-1st-Team5 sever!" })
   } catch (err) {
     console.log(err)
   }
-
 })
+
 const server = http.createServer(app)
 const start = async () => {
   try {
